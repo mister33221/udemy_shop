bfd7ceac812d9fde496e558c87c9d1bf66f71c52
Vincent <mister33221@gmail.com>
2021-08-02 21:43:39 +0800

    no message

--- spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryReepository.java
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryReepository.java
@@ -1,5 +1,7 @@
 package com.luv2code.ecommerce.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,5 +14,5 @@ import com.luv2code.ecommerce.entity.ProductCategory;
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryReepository extends JpaRepository<ProductCategory, Long> {
 
-	
+
 }
