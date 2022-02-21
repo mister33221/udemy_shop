6c0cb141838ffeed3002940502924b207779d02a
mister33221 <68389273+mister33221@users.noreply.github.com>
2022-02-21 10:05:57 +0800

    [note] AuthTokenFilter.java

--- spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/jwt/AuthTokenFilter.java
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/jwt/AuthTokenFilter.java
@@ -20,20 +20,30 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import com.luv2code.ecommerce.security.service.UserDetailsServiceImpl;
 
 
+//定義一個每一次request都會經過的filter
+//繼承OncePerRequestFilter 並且複寫doFilterInternal()
+//另外還有一個dofilter() 是整個過濾器最體層的filter的街口中的方法，所有過力氣都要實現filter
+//oncerPerRequestFilter可以確保在每一次的請求中，該次request只通過一次filter，是spring提供的抽象類別
 public class AuthTokenFilter extends OncePerRequestFilter {
-	@Autowired
+	@Autowired //沒辦法改成constructor?
 	private JwtUtils jwtUtils;
 
-	@Autowired
+	@Autowired //沒辦法改成constructor? 呼叫的時候就會一定要放參數 但改用@Autowired就不用?
 	private UserDetailsServiceImpl userDetailsService;
+	
+	
 
 	private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
 
+	
+	//doFilterInternal是onvePerRequestFilter中的一哥抽象方法
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		try {
+			System.out.println(request.authenticate(response));
 			String jwt = parseJwt(request);
+			System.out.println("jwt:"+jwt);
 			if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
 				String username = jwtUtils.getUserNameFromJwtToken(jwt);
 
@@ -51,10 +61,11 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 		filterChain.doFilter(request, response);
 	}
 
+	
 	private String parseJwt(HttpServletRequest request) {
 		String headerAuth = request.getHeader("Authorization");
 
-		if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+		if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer")) {
 			return headerAuth.substring(7, headerAuth.length());
 		}
 
