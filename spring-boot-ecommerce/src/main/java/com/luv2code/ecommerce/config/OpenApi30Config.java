6605f62358ddaa6f53cf5b18ef0338cfce8ac5da
Kai <mister33221@gmail.com>
2023-07-28 14:12:12 +0800

    [update] complete swagger header carry jwt token function

--- spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/OpenApi30Config.java
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/OpenApi30Config.java
@@ -1,5 +1,8 @@
 package com.luv2code.ecommerce.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -7,6 +10,13 @@ import io.swagger.v3.oas.annotations.info.Info;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title ="Spring boot + angular - ecommerce(udemy)", version = "v1"))
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
+)
 public class OpenApi30Config {
 
 	
--- spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/TestController.java
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.luv2code.ecommerce.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,23 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestController {
 
+	@Operation(summary = "測試公開資源", security = @SecurityRequirement(name = "Bearer Authentication"))
 	@GetMapping("/all")
 	public String allAccess() {
 		return "Public Content.";
 	}
-	
+
+	@Operation(summary = "測試用戶資源", security = @SecurityRequirement(name = "Bearer Authentication"))
 	@GetMapping("/user")
 	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public String userAccess() {
 		return "User Content.";
 	}
 
+	@Operation(summary = "測試管理員資源", security = @SecurityRequirement(name = "Bearer Authentication"))
 	@GetMapping("/mod")
 	@PreAuthorize("hasRole('MODERATOR')")
 	public String moderatorAccess() {
 		return "Moderator Board.";
 	}
 
+	@Operation(summary = "測試管理員資源", security = @SecurityRequirement(name = "Bearer Authentication"))
 	@GetMapping("/admin")
 	@PreAuthorize("hasRole('ADMIN')")
 	public String adminAccess() {
